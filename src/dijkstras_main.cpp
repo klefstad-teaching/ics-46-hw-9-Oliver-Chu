8ccd97af6e1a7cc35f0e8a6f56e01aa2c05abb7c
Oliver-Chu <ojchu@uci.edu>
2025-03-13 05:53:14 +0000

    interm save

--- src/dijkstras_main.cpp
+++ src/dijkstras_main.cpp
@@ -1,3 +1,12 @@
+#include "dijkstras.h"
+
 int main() {
-    return 0;
+    Graph G;
+    file_to_graph("/home/ojchu/ICS46/HW9/src/small.txt", G);
+    vector<int> previous(G.numVertices);
+    vector<int> distances = dijkstra_shortest_path(G, 0, previous);
+    for (int num = 0; num < G.numVertices; ++num) {
+        vector<int> path = extract_shortest_path(distances, previous, num);
+        print_path(path, distances[num]);
+    }
 }
